f57025a589a97edd8e94cf7f940798227dd68c6d
Janne Peippo <peippo@lumous.com>
2018-08-27 19:46:23 +0300

    Add input field value attribute, destructure state object

--- src/App.js
+++ src/App.js
@@ -55,18 +55,18 @@ class App extends Component {
 	}
 
 	render() {
+		const { searchTerm, list } = this.state;
 		return (
 			<div className="App">
 				<form>
 					<input
 						type="text"
+						value={searchTerm}
 						onChange={this.onSearchChange}
 					/>
 				</form>
 				{
-					this.state.list
-					.filter(isSearched(this.state.searchTerm))
-					.map(item =>
+					list.filter(isSearched(this.state.searchTerm)).map(item =>
 						<div key={item.objectID}>
 							<span>
 								<a href={item.url}>{item.title}</a>
