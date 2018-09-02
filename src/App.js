c993713ad3dfe7d007394cffc0c3c97738846565
Janne Peippo <peippo@lumous.com>
2018-09-02 20:41:53 +0300

    Refactor components as stateless functional components

--- src/App.js
+++ src/App.js
@@ -57,58 +57,44 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <Search value={searchTerm} onSearchChange={this.onSearchChange}>Search:</Search>
+        <Search value={searchTerm} onSearchChange={this.onSearchChange}>
+          Search:
+        </Search>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
 }
 
-class Button extends Component {
-  render() {
-    const { onClick, className = '', children } = this.props;
-    return (
-      <button onClick={onClick} className={className} type="button">
-        {children}
-      </button>      
-    )
-  }
-}
+const Button = ({ onClick, className = "", children }) => (
+  <button onClick={onClick} className={className} type="button">
+    {children}
+  </button>
+);
 
-class Search extends Component {
-  render() {
-    const { value, onSearchChange, children } = this.props;
-    return (
-      <form>
-        <label>{children}</label> <input type="text" value={value} onChange={onSearchChange} />
-      </form>
-    );
-  }
-}
+const Search = ({ value, onSearchChange, children }) => (
+  <form>
+    <label>{children}</label>{" "}
+    <input type="text" value={value} onChange={onSearchChange} />
+  </form>
+);
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        ))}
+const Table = ({ list, pattern, onDismiss }) => (
+  <div>
+    {list.filter(isSearched(pattern)).map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+        </span>
       </div>
-    );
-  }
-}
+    ))}
+  </div>
+);
 
 export default App;
